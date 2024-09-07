4d77cc3d9cd63b9ee0fc836f2b6b807548beeee1
alamanjs <87986282+alamanjs@users.noreply.github.com>
2024-09-07 12:45:37 +0300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,11 @@
+Config = {}
+
+Config.VerificationMethod = 'steam' -- or 'license'
+Config.VerifyBoth = true -- Set to true if u want to verify Steam and Rockstar License "Config.VerificationMethod" will be ignored in this case.
+
+-- Discord Logs
+Config.DiscordWebhookLink = 'WebHook URL'
+Config.DiscordColor = '6749952'
+Config.DiscordUserName = 'Night Scripts Anti Double Connect'
+Config.DiscordTitle = 'discord.gg/nighthost'
+Config.EnableDiscordLogs = true
